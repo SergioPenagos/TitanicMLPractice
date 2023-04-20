9c55c833989770b94db3d8662e0b20f8b328ad11
spenagos <spenagos@maxwarehouse.com>
2023-04-19 19:39:42 -0600

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Practice ML project #1',
+    author='Sergio',
+    license='',
+)
